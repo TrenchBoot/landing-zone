317ec50b1b55730008fc4e6a09d7f3661ede4a6b
Krystian Hebel <krystian.hebel@3mdeb.com>
2019-06-07 14:32:06 +0200

    setup.c: add hexdump function
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- setup.c
+++ setup.c
@@ -34,9 +34,103 @@ static __text SHA1_CONTEXT sha1ctx;
 void setup2(void);
 
 static void print(char * txt) {
-	while (*txt != '\0') print_char(*txt++);
-	print_char('\r');
-	print_char('\n');
+	while (*txt != '\0') {
+		if (*txt == '\n')
+			print_char('\r');
+		print_char(*txt++);
+	}
+}
+
+static void print_p(const void * _p) {
+	char tmp[sizeof(void*)*2 + 5] = "0x";
+	int i;
+	size_t p = (size_t)_p;
+
+	for (i=0; i<sizeof(void*); i++) {
+		if ((p & 0xf) >= 10)
+			tmp[sizeof(void*)*2 + 1 - 2*i] = (p & 0xf) + 'a' - 10;
+		else
+			tmp[sizeof(void*)*2 + 1 - 2*i] = (p & 0xf) + '0';
+		p >>= 4;
+		if ((p & 0xf) >= 10)
+			tmp[sizeof(void*)*2 - 2*i] = (p & 0xf) + 'a' - 10;
+		else
+			tmp[sizeof(void*)*2 - 2*i] = (p & 0xf) + '0';
+		p >>= 4;
+	}
+	tmp[sizeof(void*)*2 + 2] = ':';
+	tmp[sizeof(void*)*2 + 3] = ' ';
+	tmp[sizeof(void*)*2 + 4] = '\0';
+	print(tmp);
+}
+
+static void print_b(char p) {
+	char tmp[4];
+	int i;
+
+	if ((p & 0xf) >= 10)
+		tmp[1] = (p & 0xf) + 'a' - 10;
+	else
+		tmp[1] = (p & 0xf) + '0';
+	p >>= 4;
+	if ((p & 0xf) >= 10)
+		tmp[0] = (p & 0xf) + 'a' - 10;
+	else
+		tmp[0] = (p & 0xf) + '0';
+
+	tmp[2] = ' ';
+	tmp[3] = '\0';
+	print(tmp);
+}
+
+static inline int isprint(int c)
+{
+	return c >= ' ' && c <= '~';
+}
+
+void hexdump(const void *memory, size_t length)
+{
+	int i;
+	uint8_t *line;
+	int all_zero = 0;
+	int all_one = 0;
+	size_t num_bytes;
+
+	for (i = 0; i < length; i += 16) {
+		int j;
+		num_bytes = 16;
+		line = ((uint8_t *)memory) + i;
+
+		all_zero++;
+		all_one++;
+		for (j = 0; j < num_bytes; j++) {
+			if (line[j] != 0) {
+				all_zero = 0;
+				break;
+			}
+		}
+
+		for (j = 0; j < num_bytes; j++) {
+			if (line[j] != 0xff) {
+				all_one = 0;
+				break;
+			}
+		}
+
+		if ((all_zero < 2) && (all_one < 2)) {
+			print_p(memory + i);
+			for (j = 0; j < num_bytes; j++)
+				print_b(line[j]);
+			for (; j < 16; j++)
+				print("   ");
+			print("  ");
+			for (j = 0; j < num_bytes; j++)
+				isprint(line[j]) ? print_char(line[j]) : print_char('.');
+			print("\n");
+		} else if ((all_zero == 2) || (all_one == 2)) {
+			print("...\n");
+		}
+	}
 }
 
 void setup(void *_lz_base)
@@ -128,6 +222,12 @@ void setup2(void)
 	free_tpm(tpm);
 
 	/* End of the line, off to the protected mode entry into the kernel */
+	print("pm_kernel_entry:\n");
+	hexdump(pm_kernel_entry, 0x100);
+	print("zero_page:\n");
+	hexdump(zero_page, 0x100);
+	print("lz_base:\n");
+	hexdump(lz_base, 0x100);
 	lz_exit(pm_kernel_entry, zero_page, lz_base);
 
 	/* Should never get here */
