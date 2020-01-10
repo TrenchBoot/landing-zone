9b9619cb58970214beedb41c570f6ae5fb06e2fa
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-01-10 15:26:28 +0100

    iommu: Implementation of early IOMMU
    IOMMU is enabled in separate steps:
    * IOMMU is enabled, but fails to read its commands from the command
      buffer, as it is still locked by SLB,
    * IOMMU enters fail-safe state in which all device accesses are blocked,
    * DEV SLB protection is disabled,
    * IOMMU is re-enabled, this time it can read data previously protected
      by SLB.
    
    Devices can access RAM (except for SLB) before IOMMU is initially enabled,
    but not after that.
    
    This implementation uses 8 kB Device Table. 4 kB table did not cover all
    of the bus 0 devices. All devices that are not covered have master-aborted
    memory accesses (which is good), as well as interrupts (also good, most of
    the times). One of such devices was LPC, under which PIT is implemented.
    As a result, legacy timer interrupts were not received by the CPU, which
    resulted in kernel panic.
    
    Tested with legacy ISA DMA.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>
    Reviewed-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Reviewed-by: Ross Philipson <ross.philipson@oracle.com>

---
+++ include/iommu.h
@@ -0,0 +1,53 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __IOMMU_H__
+#define __IOMMU_H__
+
+typedef struct __packed {
+	u32 u0;
+	union {
+		u32 u1;
+		struct { u32 padding:28; u32 opcode:4; };
+	};
+	u32 u2;
+	u32 u3;
+} iommu_command_t;
+
+extern char device_table[2 * PAGE_SIZE];
+extern char event_log[PAGE_SIZE];
+extern iommu_command_t command_buf[PAGE_SIZE / sizeof(iommu_command_t)];
+
+u32 iommu_locate(void);
+u32 iommu_load_device_table(u32 cap, volatile u64 *completed);
+
+/* Following are used to disable initial SLB protection only */
+
+#define DEV_PCI_BUS		0x0
+#define DEV_PCI_DEVICE		0x18
+#define DEV_PCI_FUNCTION	0x3
+
+#define DEV_OP_OFFSET		4
+#define DEV_DATA_OFFSET		8
+
+#define DEV_CR			4
+
+#define DEV_CR_SL_DEV_EN_MASK	1<<5
+
+u32 dev_locate(void);
+void dev_disable_sl(u32 dev);
+
+#endif /* __IOMMU_H__ */
