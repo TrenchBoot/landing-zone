253aa55b45c5ea5d39543b0ade65c1b08bfa41ed
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-01-17 18:12:28 +0100

    util: create utility functions
        extend_lz_only.sh: print expected values of PCR17 after SKINIT, after
        they are extended with SL hash only (both SHA1 and SHA256).
    
        extend_all.sh: takes path to bzImage and initrd (in that order) as
        arguments and calculates values expected after extending with LZ,
        kernel and initrd.
    
        sanity_check.sh: checks for LZ UUID at the correct offset, will be used
        by the following commit.
    
        util.sh: helper functions for above scripts.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

---
+++ extend_lz_only.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. util.sh
+
+echo "Initial extensions of PCR17 after SKINIT for ${SLB_FILE}:"
+extend_sha1
+extend_sha256
---
+++ util.sh
@@ -0,0 +1,85 @@
+SLB_FILE=${SLB_FILE:=lz_header.bin}
+
+SL_SIZE=`hexdump "$SLB_FILE" -s2 -n2 -e '/2 "%u"'`
+
+validate_and_escape_hash () {
+	local TRIM=`echo -n "$1" | sed -r -e "s/ .*//"`
+	if (( ${#TRIM} != 64 && ${#TRIM} != 40 )); then
+		>&2 echo "\"$TRIM\" is not a valid SHA1/SHA256 hash"
+		return
+	fi
+	echo -n $TRIM | sed -r -e "s/([a-f0-9]{2})/\\\\\x\1/g"
+}
+
+# extend_sha* - extend initial PCR value (all 0s) with ${FILE}'s hash (SL only)
+# extend_sha* file.bin - extend initial PCR value with full file.bin's hash
+# extend_sha* HASH file.bin - extend PCR with value=HASH with file.bin's hash
+# extend_sha* HASH1 HASH2 - extend PCR with value=HASH1 with HASH2
+# extend_sha* HASH file file...
+# extend_sha* HASH HASH HASH... - as long as file is not first
+extend_sha1 () {
+	#echo $# >&2
+	local HASH1
+	local HASH2
+	if [ $# -eq 0 ]; then
+		HASH1=`printf "0%.0s" {1..40}`
+		HASH2=`dd if="$SLB_FILE" bs=1 count=$SL_SIZE 2>/dev/null | sha1sum`
+	elif [ $# -eq 1 ]; then
+		if [ -f "$1" ]; then
+			HASH1=`printf "0%.0s" {1..40}`
+			HASH2=`dd if="$1" 2>/dev/null | sha1sum`
+		else
+			HASH1=`printf "0%.0s" {1..40}`
+			HASH2="$1"
+		fi
+	elif [ $# -eq 2 ]; then
+		if [ -f "$2" ]; then
+			HASH1="$1"
+			HASH2=`dd if="$2" 2>/dev/null | sha1sum`
+		else
+			HASH1="$1"
+			HASH2="$2"
+		fi
+	else
+		HASH1=$(extend_sha1 "$1" "$2")
+		shift 2
+		extend_sha1 "$HASH1" $@
+		return
+	fi
+	local HASH1_ESC=$(validate_and_escape_hash "$HASH1")
+	local HASH2_ESC=$(validate_and_escape_hash "$HASH2")
+	printf "%b" $HASH1_ESC $HASH2_ESC | sha1sum | sed "s/-/SHA1/"
+}
+
+extend_sha256 () {
+	local HASH1
+	local HASH2
+	if [ $# -eq 0 ]; then
+		HASH1=`printf "0%.0s" {1..64}`
+		HASH2=`dd if="$SLB_FILE" bs=1 count=$SL_SIZE 2>/dev/null | sha256sum`
+	elif [ $# -eq 1 ]; then
+		if [ -f "$1" ]; then
+			HASH1=`printf "0%.0s" {1..64}`
+			HASH2=`dd if="$1" 2>/dev/null | sha256sum`
+		else
+			HASH1=`printf "0%.0s" {1..64}`
+			HASH2="$1"
+		fi
+	elif [ $# -eq 2 ]; then
+		if [ -f "$2" ]; then
+			HASH1="$1"
+			HASH2=`dd if="$2" 2>/dev/null | sha256sum`
+		else
+			HASH1="$1"
+			HASH2="$2"
+		fi
+	else
+		HASH1=$(extend_sha256 "$1" "$2")
+		shift 2
+		extend_sha256 "$HASH1" $@
+		return
+	fi
+	local HASH1_ESC=$(validate_and_escape_hash "$HASH1")
+	local HASH2_ESC=$(validate_and_escape_hash "$HASH2")
+	printf "%b" $HASH1_ESC $HASH2_ESC | sha256sum | sed "s/-/SHA256/"
+}
