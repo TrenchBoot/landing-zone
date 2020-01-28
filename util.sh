c8e39fd3385782967cfd14e59ffa688ad8b71f8e
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-01-28 15:35:52 +0100

    utils: post-review fixes
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- util.sh
+++ util.sh
@@ -8,7 +8,7 @@ validate_and_escape_hash () {
 		>&2 echo "\"$TRIM\" is not a valid SHA1/SHA256 hash"
 		return
 	fi
-	echo -n $TRIM | sed -r -e "s/([a-f0-9]{2})/\\\\\x\1/g"
+	echo -n $TRIM | sed -r -e "s/([a-f0-9]{2})/\\\x\1/g"
 }
 
 # extend_sha* - extend initial PCR value (all 0s) with ${FILE}'s hash (SL only)
@@ -18,34 +18,34 @@ validate_and_escape_hash () {
 # extend_sha* HASH file file...
 # extend_sha* HASH HASH HASH... - as long as file is not first
 extend_sha1 () {
-	#echo $# >&2
 	local HASH1
 	local HASH2
-	if [ $# -eq 0 ]; then
-		HASH1=`printf "0%.0s" {1..40}`
+	case $# in
+	0)	HASH1=`printf "0%.0s" {1..40}`
 		HASH2=`dd if="$SLB_FILE" bs=1 count=$SL_SIZE 2>/dev/null | sha1sum`
-	elif [ $# -eq 1 ]; then
-		if [ -f "$1" ]; then
+		;;
+	1 )	if [ -f "$1" ]; then
 			HASH1=`printf "0%.0s" {1..40}`
 			HASH2=`dd if="$1" 2>/dev/null | sha1sum`
 		else
 			HASH1=`printf "0%.0s" {1..40}`
 			HASH2="$1"
 		fi
-	elif [ $# -eq 2 ]; then
-		if [ -f "$2" ]; then
+		;;
+	2 )	if [ -f "$2" ]; then
 			HASH1="$1"
 			HASH2=`dd if="$2" 2>/dev/null | sha1sum`
 		else
 			HASH1="$1"
 			HASH2="$2"
 		fi
-	else
-		HASH1=$(extend_sha1 "$1" "$2")
+		;;
+	* )	HASH1=$(extend_sha1 "$1" "$2")
 		shift 2
 		extend_sha1 "$HASH1" $@
 		return
-	fi
+		;;
+	esac
 	local HASH1_ESC=$(validate_and_escape_hash "$HASH1")
 	local HASH2_ESC=$(validate_and_escape_hash "$HASH2")
 	printf "%b" $HASH1_ESC $HASH2_ESC | sha1sum | sed "s/-/SHA1/"
