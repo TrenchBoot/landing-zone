936d30c351f85e75cb6fd732cfa6601a6e703e6a
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-01-28 15:35:52 +0100

    utils: post-review fixes
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- sanity_check.sh
+++ sanity_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . util.sh
 
-od --format=x8 --skip-bytes=$SL_SIZE --read-bytes=16 $SLB_FILE | \
-	grep "e91192048e26f178 02ccc4765bc82a83" > /dev/null || \
-	{ echo "ERROR: LZ UUID missing or misplaced in $SLB_FILE" >&2; false; }
-
+if ! od --format=x8 --skip-bytes=$SL_SIZE --read-bytes=16 $SLB_FILE | grep -q "e91192048e26f178 02ccc4765bc82a83"; then
+    echo "ERROR: LZ UUID missing or misplaced in $SLB_FILE" >&2
+    false
+fi
