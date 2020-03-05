55ac466db91885e2035ce487c680c2a6f104d54f
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-03-05 19:52:15 +0100

    sha256.c: make K static
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- sha256.c
+++ sha256.c
@@ -61,7 +61,7 @@ static inline u32 BLEND_OP(u32 *W)
 	return ret;
 }
 
-const u32 K[] = {
+static const u32 K[] = {
 	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,
 	0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
 	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,
