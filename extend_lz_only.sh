4dc904bf70d6913a5706bdfc6f7781a9ff039d38
Krystian Hebel <krystian.hebel@3mdeb.com>
2020-07-21 15:01:08 +0200

    extend_all.sh: support kernels with embedded initramfs
    Modify test for number of command line arguments. For one argument,
    measure the file pointed by that argument assuming it is a Linux kernel
    and simulate a PCR extend operation for just the LZ and the kernel's hash.
    
    This commit also refactors code for extend_shaXX functions and adds more
    helpers - separate functions for calculating hashes of measured parts of
    LZ and kernel.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- extend_lz_only.sh
+++ extend_lz_only.sh
@@ -2,5 +2,5 @@
 . util.sh
 
 echo "Initial extensions of PCR17 after SKINIT for ${SLB_FILE}:"
-extend_sha1
-extend_sha256
+extend_sha1 "$sha1_zeroes" $(sha1_lz)
+extend_sha256 "$sha256_zeroes" $(sha256_lz)
